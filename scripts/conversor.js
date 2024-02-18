d0957eab726ecfd9744b2984702d73b692910a47
DahbOne1 <david.arcos1000@gmail.com>
2024-02-17 20:23:47 -0600

    final version

--- scripts/conversor.js
+++ scripts/conversor.js
@@ -1,5 +1,7 @@
 //Variables para hexadecimal
 const hexDigitos = '0123456789ABCDEF';
+//Variables para octales
+const octalDigits = ['000', '001', '010', '011', '100', '101', '110', '111'];
 
 
 //Funcion para realizar las conversiones de a los demas sistemas numericos
@@ -27,7 +29,13 @@ function convertir(){
             if(numeroAConvertir == 0){
                 binario = 0;
                 octal = 0;
+                decimal = 0;
                 hexadecimal = 0;
+                //Ingresando los resultados a la tabla
+                inputBinario.innerText = binario;
+                inputOctal.innerText = octal;
+                inputDecimal.innerText = decimal;
+                inputHexadecimal.innerText = hexadecimal;
             }else{
                 inputDecimal.innerText = numeroAConvertir;
                 //Decimal a binario
@@ -63,9 +71,15 @@ function convertir(){
         //Conversiones de binario a otros sistemas numericos
         case "BO":
             if(numeroAConvertir == 0){
-                decimal = 0;
+                binario = 0;
                 octal = 0;
+                decimal = 0;
                 hexadecimal = 0;
+                //Ingresando los resultados a la tabla
+                inputBinario.innerText = binario;
+                inputOctal.innerText = octal;
+                inputDecimal.innerText = decimal;
+                inputHexadecimal.innerText = hexadecimal;
             }else{
                 inputBinario.innerText = numeroAConvertir;
                 
@@ -85,19 +99,76 @@ function convertir(){
         break;
         //Conversiones de octal a otros sistemaas numericos
         case "OL":
-            //Octal a decimal
-    
-            //octal a binario
-    
-            //octal a hexadecimal
+            if(numeroAConvertir == 0){
+                binario = 0;
+                octal = 0;
+                decimal = 0;
+                hexadecimal = 0;
+                //Ingresando los resultados a la tabla
+                inputBinario.innerText = binario;
+                inputOctal.innerText = octal;
+                inputDecimal.innerText = decimal;
+                inputHexadecimal.innerText = hexadecimal;
+            }else{
+                inputOctal.innerText = numeroAConvertir;
+                
+                //Octal a binario
+                let numABinario = numeroAConvertir;
+                // Convertir cada dígito octal a binario
+                for (let i = 0; i < numABinario.length; i++) {
+                    let octalDigito = parseInt(numABinario[i], 8); // Convertir el dígito octal a decimal
+                    let binarioDigito = octalDigits[octalDigito]; // Obtener el equivalente binario del dígito octal
+                    binario += binarioDigito;
+                }
+
+                // Eliminar ceros no significativos al principio
+                binario = binario.replace(/^0+/, '');
+                inputBinario.innerText = binario;
+
+                //Octal a decimal
+                let numADecimal = numeroAConvertir;
+                decimal = parseInt(numADecimal, 8);
+                inputDecimal.innerText = decimal;
+
+                //Octal a hexadecimal 
+                let numAHexa = numeroAConvertir;
+                let hexa = parseInt(numAHexa,8);
+                hexadecimal = hexa.toString(16).toUpperCase();
+                inputHexadecimal.innerText = hexadecimal;
+            }
         break;
         //Conversiones de hexadecimal a otros sistemas numericos
         case "HL":
-            //Hexadecimal a decimal
-    
-            //Hexadecimal a binario
-    
-            //Hexadecimal a octal
+            if(numeroAConvertir == 0){
+                binario = 0;
+                octal = 0;
+                decimal = 0;
+                hexadecimal = 0;
+                //Ingresando los resultados a la tabla
+                inputBinario.innerText = binario;
+                inputOctal.innerText = octal;
+                inputDecimal.innerText = decimal;
+                inputHexadecimal.innerText = hexadecimal;
+            }else{
+                inputHexadecimal.innerText = numeroAConvertir;
+                
+                //Hexadecimal a binario
+                let numABinario = numeroAConvertir;
+                let bin = parseInt(numABinario,16);
+                binario = bin.toString(2);
+                inputBinario.innerText = binario;
+
+                //Hexadecimal a octal
+                let numAOctal = numeroAConvertir;
+                let oct = parseInt(numAOctal,16);
+                octal = oct.toString(8);
+                inputOctal.innerText = octal;
+
+                //Hexadecimal a decimal 
+                let numADecimal = numeroAConvertir;
+                decimal = parseInt(numADecimal,16);
+                inputDecimal.innerText = decimal;
+            }
         break;
     
     
